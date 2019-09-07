a3a979221d3f2eae0ce32055d3648b1d4605b2db
Augusto Martinez <acm_asd@hotmail.com>
2019-09-07 16:01:37 -0600

    updates

--- js/media-sub.js
+++ js/media-sub.js
@@ -20,18 +20,55 @@ function run() {
  
  for (let i = 0; i < elems.length; i++) {
    let styles = getComputedStyle(elems[i], null);
+   let befores = getComputedStyle(elems[i], "::before");
+   let afters = getComputedStyle(elems[i], "::after");
    
    if (styles.backgroundImage !== "none") {
-     let imgSrc = styles.backgroundImage;
-     let regEx = /url\((.*?)\)/;
-     let remEx = /\"|\'|media_src_|\%5E/g;
-     
-     let str = regEx.exec(imgSrc);     
-     let arr = str[1].split("/");
-     
-     str = arr[(arr.length - 1)].replace(remEx,"");     
-     elems[i].style.backgroundImage = "url('img/" + str + "')";
+     let imgSrc = styles.backgroundImage;     
+     elems[i].style.backgroundImage = regExExecute(imgSrc);
    }
- }
+   
+   if (befores.backgroundImage !== "none") {
+     let imgSrc = befores.backgroundImage;
+     str = regExExecute(imgSrc);
+     
+     let rule = "*[data-before=\"style-"+ i +"\"]::before {\n\
+                   background-image:"+ str + "!important;\n\
+                   \}";
+                   
+     createCSS(rule);
+     elems[i].setAttribute("data-before", "style-" + i);
+   }
+   
+   if (afters.backgroundImage !== "none") {
+     let imgSrc = afters.backgroundImage;
+     str = regExExecute(imgSrc);
+     
+     let rule = "*[data-after=\"style-"+ i +"\"]::after {\n\
+                   background-image:"+ str + "!important;\n\
+                   \}";
+     
+     createCSS(rule);
+     elems[i].setAttribute("data-after", "style-" + i);
+   }
+ }  
+}
+
+function regExExecute(imgSrc) {
+  let regEx = /url\((.*?)\)/;
+  let remEx = /\"|\'|media_src_|\%5E/g;
+     
+  let str = regEx.exec(imgSrc);     
+  let arr = str[1].split("/");
+     
+  str = arr[(arr.length - 1)].replace(remEx,"");     
   
+  return "url('img/" + str + "')";;
+}
+
+function createCSS(rule) {
+  let css = document.createElement("style");
+  css.type = "text/css";
+  css.appendChild(document.createTextNode(rule));
+  document.getElementsByTagName("head")[0].appendChild(css);
 }
